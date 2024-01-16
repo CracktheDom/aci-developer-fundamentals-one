28c5fe9044aa56f7bb80200cb3bffa34e8a9d9f9
CrackThaDom <85846263+CracktheDom@users.noreply.github.com>
2024-01-15 18:21:39 -0600

    Add comments & docstrings to logging_challenge.py

--- week 07 Logging/logging_challenge.py
+++ week 07 Logging/logging_challenge.py
@@ -2,28 +2,50 @@ import logging
 
 
 def process_data() -> list[int]:
+    """
+    Process user input to create a list of integers.
+
+    Returns:
+    - list[int]: A list of integers entered by the user.
+
+    Note:
+    - The user is prompted to enter a list of numbers separated by commas.
+    - If the input is not comma-separated, the user is prompted to try again
+    until a valid input is provided.
+    """
     logging.info("The function has started")
     response: str = input(
         "Enter a list of numbers separated by commas, i.e. 5, 8, 6, 92: "
     )
     if "," not in response:
         input_is_valid = False
-        logging.error("not comma separated values entered")
+        logging.error("Not comma-separated values entered")
         while not input_is_valid:
-            response = input("Try again ")
+            response = input("Try again: ")
             if "," in response:
                 input_is_valid = True
             else:
                 input_is_valid = False
-                logging.error("still no comma separated values entered")
+                logging.error("Still no comma-separated values entered")
     else:
-        input_is_valid = True
+        input_is_valid = True  # valid input initially was entered
+
+        """
+        Strip blank space from string & split characters between commas into
+        a list
+        """
         num_list = [int(num) for num in response.replace(" ", "").split(",")]
-        logging.info(f"The numbers of integers inputted is {len(num_list)}")
+
+        # Log information about numbers entered
+        logging.info(f"The number of integers inputted is {len(num_list)}")
         logging.info(f"The function returns {num_list}")
     return num_list
 
 
+"""
+Set up logging behavior to append INFO and higher level messages along with
+time, date, & log level to 'filename'
+"""
 logging.basicConfig(
     filename="example.log",
     filemode="a",
