f3df5e2cc7a51b529b720816acaf9a8c1700fb5d
CrackThaDom <85846263+CracktheDom@users.noreply.github.com>
2024-01-18 11:25:13 -0600

    Add comments & docstrings & reformat code in sales_script.py

--- week 05 libraries/sales_script.py
+++ week 05 libraries/sales_script.py
@@ -4,6 +4,49 @@ from product_sales import Product, Sales
 import random
 
 
+def create_product_objects(data: list[tuple]) -> list[Product]:
+    """
+    Create a list of Product objects based on the provided data.
+
+    Args:
+    - data (list[tuple]): A list of tuples containing product information (name, price, quantity).
+
+    Returns:
+    - list[Product]: A list of Product objects.
+
+    Note:
+    - Each tuple in the data should have three elements: name (str), price (float), quantity (int).
+    """
+    products_list = []
+    for product in product_data:
+        name, price, quant = product
+        products_list.append(Product(name, price, quant))
+    return products_list
+
+
+def report_sales(product_list: list[Product]) -> str:
+    """
+    Generate a sales report for a list of Product objects.
+
+    Args:
+    - product_list (list[Product]): A list of Product objects.
+
+    Returns:
+    - str: A formatted sales report.
+
+    Note:
+    - Sales are randomly generated for every second product in the list.
+    """
+    sales = Sales()  # create Sales object
+
+    # Iterate through product_list and generate random sales data
+    for index, product_elem in enumerate(product_list):
+        if index % 2 == 0:
+            rand_num = random.randint(1, 10)
+            sales.add_sale(product_elem, rand_num)
+    return sales.generate_report()
+
+
 product_data: list[tuple] = [
     ("widgetA", 5.99, 100),
     ("widgetB", 10.99, 100),
@@ -17,26 +60,13 @@ product_data: list[tuple] = [
     ("widgetJ", 13.99, 50),
 ]
 
+# Generate random integers for with sales data
 x = random.randint(1, 10)
 
 
-def create_product_objects(data: list[tuple]) -> list[Product]:
-    products = []
-    for product in product_data:
-        name, price, quant = product
-        products.append(Product(name, price, quant))
-    return products
-
-
-def report_sales(product_list: list[Product]):
-    sales = Sales()
-    for index, product_elem in enumerate(product_list):
-        if index % 2 == 0:
-            rand_num = random.randint(1, 10)
-            sales.add_sale(product_elem, rand_num)
-    return sales.generate_report()
-
-
 if __name__ == "__main__":
+    # Create Product objects from the provided data
     batch1 = create_product_objects(product_data)
+
+    # Generate and print a sales report for the created products
     print(report_sales(batch1))
