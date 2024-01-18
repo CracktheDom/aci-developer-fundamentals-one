8ffda1ce83bd2f46a6920fa61e9f0d465d7b0e1e
CrackThaDom <85846263+CracktheDom@users.noreply.github.com>
2024-01-18 14:32:41 -0600

    moved file ice_cream_order.py

--- week 02 Data Structures/ice_cream_order.py
+++ week 02 Data Structures/ice_cream_order.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """For this task, imagine that you are the proud owner of an ice cream shop. As
 an entrepreneur, you know that increased efficiency can be the thing that sets
@@ -20,15 +20,38 @@ customers to order multiple cones or cups, with a different flavor possible for
 each scoop. If a customer enters an invalid option, notify them of the error
 and prompt them again."""
 
+# constant variables
 SERVING_CHOICE = (cone, cup)
 NUMBER_OF_SCOOPS = (1, 2, 3)
-FLAVOR = (vanilla, strawberry, chocolate, cherry, mint, peach, grape)
+FLAVORS = (vanilla, strawberry, chocolate, cherry, mint, peach, grape)
 
+# Initialize variables
 order_id = 0
-is_finished = False
-is_valid_input = False
+isOrderComplete = False
+isValidInput = False
 
 print("Greetings, how can I help you today?")
-while not is_finished:
+while not isOrderComplete:
 
 
+    # start the order probably use while loop
+
+    # prompt for input for container type, while loop until valid input
+    # validate container input
+    # add container response to order variable, possibly dict
+    # invalid input => print error msg & restart loop
+
+    # prompt for input of # of scoops
+    # validate scoop response
+    # add scoop response to order variable
+    # invalid input => print error msg & restart loop
+
+    # iterate over # of scoops
+    # prompt for input of # of flavor(s)
+    # validate flavor(s) response
+    # add flavor(s) response to order variable
+    # invalid input => print error msg & restart loop
+
+    # prompt if order is complete
+    # if yes, change flag, exit order while loop, display details of order
+    # if no, restart order while loop
