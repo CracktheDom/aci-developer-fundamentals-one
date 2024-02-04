4d0949b7bede25662ae13189c69be225fbcef65d
CrackThaDom <85846263+CracktheDom@users.noreply.github.com>
2024-02-04 00:14:35 -0600

    Refactor code in 01_lab_multiplier.py

--- week 01 Python Overview/01_lab_multiplier.py
+++ week 01 Python Overview/01_lab_multiplier.py
@@ -2,23 +2,21 @@
 # and prints a formatted string of the response to the user.
 
 # Get first integer from user
-number1 = input("Please enter your first integer: ")
-if number1.isnumeric():
-    number1 = int(number1)
-else:
-    while not number1.isnumeric():
-        number1 = input("Please enter a valid integer: ")
+while True:
+    try:
+        first_number = int(input("Please enter your first integer: "))
+        break
+    except ValueError:
+        print("Please enter a valid integer\n")
 
 # Get second integer from user
-number2 = input("Please enter your second integer: ")
-if number2.isnumeric():
-    number2 = int(number2)
-else:
-    while not number2.isnumeric():
-        number2 = input("Please enter a valid integer: ")
+while True:
+    try:
+        second_number = int(input("Please enter your second integer: "))
+        break
+    except ValueError:
+        print("Please enter a valid integer\n")
 
-# Calculate the product of two inputs
-product = int(number1) * int(number2)
 
-# Print out the result
-print(f"The product of your integers is {product}.")
+# Calculate the product of two inputs & print out the result
+print(f"The product of your integers is {int(first_number) * int(second_number):,}.")
